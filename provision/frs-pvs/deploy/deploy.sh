c7d3582dc37a7cfe993e291f81b721d0901961a0
ballgaveship <ballgaveship@gmail.com>
2020-10-08 00:24:14 +0900

    add command line

--- provision/frs-pvs/deploy/deploy.sh
+++ provision/frs-pvs/deploy/deploy.sh
@@ -1,1 +1,5 @@
-echo test
+echo $GITHUB_PACKAGES_PASSWORD | docker login docker.pkg.github.com -u ballgaveship --password-stdin
+docker-compose stop
+docker-compose rm -f
+docker-compose pull
+docker-compose up -d
