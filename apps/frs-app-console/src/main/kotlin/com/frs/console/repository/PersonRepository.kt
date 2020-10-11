5207137541c671776507035e57cd1067913635eb
ballgaveship <ballgaveship@gmail.com>
2020-10-11 10:43:45 +0900

    ReactiveCrudRepository -> ReactiveSortingRepository Remove query annotation not required

--- apps/frs-app-console/src/main/kotlin/com/frs/console/repository/PersonRepository.kt
+++ apps/frs-app-console/src/main/kotlin/com/frs/console/repository/PersonRepository.kt
@@ -1,13 +1,11 @@
 package com.frs.console.repository
 
 import com.frs.console.domain.person.Person
-import org.springframework.data.r2dbc.repository.Query
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.repository.reactive.ReactiveSortingRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
 
 @Repository
-interface PersonRepository : ReactiveCrudRepository<Person, Int> {
-    @Query("select p.* from person p where p.name=:name")
+interface PersonRepository : ReactiveSortingRepository<Person, Int> {
     fun findByName(name: String): Flux<Person>
 }
